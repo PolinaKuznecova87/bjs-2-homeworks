595ba862e5532b8f260a1bdd817bbc5c6eba8a51
Polina K <polinav_altay@mail.ru>
2023-11-12 16:39:09 +0300

    add task fale

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d === 0) {
+
+		arr.push(-b / (2 * a));
+
+	} else if (d > 0) {
+
+		arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+
+	}
+	return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+
+	let p = ((percent / 100) / 12);
+	let s = (amount - contribution);
+	let m = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let total = m * countMonths;
+	return +total.toFixed(2);
+
 }
