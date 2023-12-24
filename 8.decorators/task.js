2b3355c59f66a84b18ca11194e6e960a66a99c6d
Polina K <polinav_altay@mail.ru>
2023-12-24 17:08:45 +0300

    add fale decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,61 @@
 //Задача № 1
+
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+
+		const hash = md5(JSON.stringify(args));
+
+		let objectCache = cache.find(item => item.hash === hash); //ищем элементы в кеше по хешу
+		if (objectCache) {
+
+			console.log("Из кеша: " + objectCache.value, cache);
+			return "Из кеша: " + objectCache.value;
+		}
+		let result = func(...args);
+
+		cache.push({
+			hash: hash,
+			value: result
+		});
+
+		if (cache.length > 5) {
+
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result, cache);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
-//Задача № 2
+
+
+//Задание 2
+
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+	let count = 0; //количество апли
+	let allCount = 0; //количество всех оберток
+
+	function wrapper(...args) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+			allCount++;
+		}
+
+		if (!timeoutId) {
+			func.apply(this, args);
+			count++;
+		}
+		timeoutId = setTimeout(() => {
+			count++;
+			func.apply(this, args);
+			wrapper.count = count;
+		}, delay);
+
+		wrapper.allCount = allCount;
+	}
+	return wrapper;
 }
